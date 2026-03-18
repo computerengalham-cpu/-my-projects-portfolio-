78e11ebe000134c9850aa2bcbdbcc6a33040ba4c
computerengalham-cpu <computerengalham@gmail.com>
2026-03-18 14:44:37 +0400

    add directives (ngIf,ngFor,ngSwitch)and routing

--- project1/src/app/app.module.ts
+++ project1/src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule,Routes } from '@angular/router';
 
+const routes: Routes =[
+  {path:'',component:HomeComponent},
+  {path:'profile',component:ProfileComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
